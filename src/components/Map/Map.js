eb8d61abd9a24489ebfb1198bd74ed736a9e88d5
zhangyiqi <yiqi@animo.ai>
2021-10-31 12:00:44 -0400

    initial googlemaps rendered

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,31 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
 
 // Styles
 import * as S from "./Map.styles";
 
 const Map = () => {
-  return <S.MapContainer> HI </S.MapContainer>;
+  const { REACT_APP_API_KEY } = process.env;
+  const handleApiLoaded = (map, maps) => {
+    // use map and maps objects
+  };
+
+  // Toronto
+  const center = {
+    lat: 43.65107,
+    lng: -79.347015,
+  };
+  return (
+    <S.MapContainer>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: REACT_APP_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={11}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+      ></GoogleMapReact>{" "}
+    </S.MapContainer>
+  );
 };
 
 export default Map;
--- src/components/Map/Map.styles.js
+++ src/components/Map/Map.styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const MapContainer = styled.div`
   display: flex;
-  background: pink;
   width: 100%;
   height: 100vh;
 `;
