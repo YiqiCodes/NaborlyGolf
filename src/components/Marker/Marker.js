f64c7d8611aa974526d40c041dca7b981624aeca
zhangyiqi <yiqi@animo.ai>
2021-11-03 09:54:04 -0400

    added tailwind modal and more properties

--- src/components/Marker/Marker.js
+++ src/components/Marker/Marker.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
-// Antd
-import { Modal } from "antd";
+// Assets
+import { HomeIcon } from "@heroicons/react/outline";
 
 // Styles
 import "./Marker.css";
 import "antd/dist/antd.css";
-import * as S from "./Marker.styles";
+
+// Tailwind
+import { Dialog, Transition } from "@headlessui/react";
 
 const Marker = ({
   color,
@@ -19,15 +21,16 @@ const Marker = ({
   name,
   totalViews,
 }) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const showModal = () => {
-    setIsModalVisible(true);
+    setOpen(true);
   };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
   return (
     <>
@@ -41,29 +44,80 @@ const Marker = ({
         />
         <div className="pulse" />
       </div>
-      <Modal
-        visible={isModalVisible}
-        centered
-        footer={null}
-        keyboard={true}
-        closable={true}
-        bodyStyle={{
-          padding: 0,
-        }}
-        width={"50%"}
-        onCancel={handleCancel}
-      >
-        <S.ModalContainer>
-          <div>Property Name: {name}</div>
-          <div>City: {city}</div>
-          <div>Country: {country}</div>
-          <div>Lease Term: {leaseTermMonths}</div>
-          <div>Monthly Rate: {monthlyRate}</div>
-          <div>Latitude: {lat}</div>
-          <div>Longitude: {lng}</div>
-          <div>Total Views: {totalViews}</div>
-        </S.ModalContainer>
-      </Modal>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    <HomeIcon className="h-6 w-6 text-600" aria-hidden="true" />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      {name}
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        <div>City: {city}</div>
+                        <div>Country: {country}</div>
+                        <div>Lease Term: {leaseTermMonths}</div>
+                        <div>Monthly Rate: {formatter.format(monthlyRate)}</div>
+                        <div>Latitude: {lat}</div>
+                        <div>Longitude: {lng}</div>
+                        <div>Total Views: {totalViews}</div>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Go back
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
