cd1fc3dead42b5fee5989c3f48c92b3d57068dff
zhangyiqi <yiqi@animo.ai>
2021-11-06 14:01:52 -0400

    final touchups

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -14,20 +14,18 @@ export default function HomePage() {
           aria-hidden="true"
           className="absolute inset-0 bg-gray-900 bg-opacity-50"
         />
-        <div className="relative max-w-3xl mx-auto flex flex-col items-center text-center">
+        <div className="relative max-w-3xl w-3/5 mx-auto flex flex-col items-center text-center">
           <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
             NaborlyGolf
           </h2>
           <p className="mt-3 text-xl text-white">
-            Browse nearby courses, view products and watch instrucitonal videos
-            to help improve your game. More features coming soon!
+            Browse nearby courses, view products and watch instructional videos
+            to help improve your game.
           </p>
-          {/* <a
-            href="#"
-            className="mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-          >
-            Read our story
-          </a> */}
+          <p className="mt-1 text-m text-white">
+            Built with React, antd, axios, react-player, styled-components, swr,
+            tailwindCSS, CoinDesk API & Google Maps API.
+          </p>
         </div>
       </div>
     </div>
