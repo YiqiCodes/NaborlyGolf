5afbf5f63368fdbaf11e9c75364e7ff8bd798f90
zhangyiqi <yiqi@animo.ai>
2021-11-06 14:25:43 -0400

    updated tooltip for title page

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,9 @@
+// Antd
+import { Tooltip } from "antd";
+
+// Assets
 import golfCoursePhoto from "../../assets/golfcourse.jpg";
+
 export default function HomePage() {
   return (
     <div className="flex items-center w-full relative bg-white">
@@ -14,19 +19,21 @@ export default function HomePage() {
           aria-hidden="true"
           className="absolute inset-0 bg-gray-900 bg-opacity-50"
         />
-        <div className="relative max-w-3xl w-4/6 mx-auto flex flex-col items-center text-center">
-          <h2 className="text-6xl font-extrabold tracking-tight text-white">
-            NaborlyGolf
-          </h2>
-          <p className="mt-3 text-2xl text-white">
-            Browse nearby courses, view products and watch instructional videos
-            to help improve your game.
-          </p>
-          <p className="mt-1 text-base text-white">
-            Built with React, antd, axios, react-player, styled-components, swr,
-            tailwindCSS, CoinDesk API & Google Maps API.
-          </p>
-        </div>
+        <Tooltip
+          placement="top"
+          title="Built with React, antd, axios, react-player, styled-components, swr,
+            tailwindCSS, CoinDesk API & Google Maps API."
+        >
+          <div className="cursor-help relative max-w-3xl w-4/6 mx-auto flex flex-col items-center text-center">
+            <h2 className="text-6xl font-extrabold tracking-tight text-white">
+              NaborlyGolf
+            </h2>
+            <p className="mt-5 text-2xl text-white">
+              Browse nearby courses, view products and watch instructional
+              videos to help improve your game.
+            </p>
+          </div>
+        </Tooltip>
       </div>
     </div>
   );
