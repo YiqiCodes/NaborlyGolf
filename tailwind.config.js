b1d22d304c46ae29b773a87fc0c11cc6923bc19e
Yiqi Zhang <yiqi@Yiqis-MacBook-Pro.local>
2021-12-14 18:57:05 -0500

    iphone 12 mini fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: "400px",
+      sm: "375px",
       // => @media (min-width: 400px) { ... }
 
       md: "700px",
